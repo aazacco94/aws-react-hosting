443e6aad38b1631f4845a33c9d48647f0b788d6a
Alex Zacco <zaccoa94@gmail.com>
2022-06-28 15:11:10 -0400

    '

--- src/components/BannerSection/Data.js
+++ src/components/BannerSection/Data.js
@@ -4,11 +4,13 @@ export const BannerData = {
   id: 'home',
   videolink: bannerVid,
   header: 'Zacco Software Enterprise',
-  desc1: 'Welcome to the website showcase towards the programmatic abilities of the one and only Alex Zacco.'+
+  desc1: 'Welcome, this webite acts as a showcase for the programming abilities of myself, Alex Zacco.'+
           ' All interested employers can find my resume below in the Certifications section.'+
           ' All finished demos are under the Projects section and I will be adding to this list over time.'+
-          ' Click on the tabs in the Navigation Bar above to directed accordingly.',
-  desc2: 'All gamer enthusiests can watch gameplay from various popular titles under the Gameplay section.',
+          ' All gamer enthusiests can watch gameplay from various popular titles under the Gameplay section.',
+
+   desc2: '  Mobile: Click side bar menu on the upper right corner.'+
+        ' Click on the tabs in the Navigation Bar above to directed accordingly.',
   buttonLabel: 'Learn More',
   buttonLink: 'about',
   lightText: true
